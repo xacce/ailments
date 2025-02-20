ae3fb0f53ba7883911ed1a5e039d4d555166f91d
Xacce <mmodbrreciever@gmail.com>
2025-02-21 01:08:35 +0300

    more authorings

---
+++ Hybrid/AilmentRuntimeStorageAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using Src.PackageCandidate.Ailments.Runtime;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Src.PackageCandidate.Ailments.Hybrid
+{
+    public class AilmentRuntimeStorageAuthoring : MonoBehaviour
+    {
+        class _ : Baker<AilmentRuntimeStorageAuthoring>
+        {
+            public override void Bake(AilmentRuntimeStorageAuthoring authoring)
+            {
+                var e = GetEntity(TransformUsageFlags.None);
+                AddBuffer<ApplyAilment>(e);
+                // AddComponent(new AilmentRuntimeStorageAuthoring{},e);
+                // AddBuffer<AilmentRuntimeStorageAuthoring>(e)
+            }
+        }
+    }
+}
