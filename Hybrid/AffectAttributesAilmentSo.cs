cc8b7ef5e8af235c531bb541f319383dbd6bfc4a
Xacce <mmodbrreciever@gmail.com>
2025-03-12 18:29:26 +0300

    abstract

--- Hybrid/AffectAttributesAilmentSo.cs
+++ Hybrid/AffectAttributesAilmentSo.cs
@@ -10,7 +10,7 @@ namespace Src.PackageCandidate.Ailments.Hybrid
     [CreateAssetMenu(menuName = "Sufferenger/Ailments/Affect attrs ailment")]
     public class AffectAttributesAilmentSo : AilmentBakedSo
     {
-        [SerializeField] private AttributeValuesPresetSo affectAttributes;
+        [SerializeField] private AttributeValuesArrayPresetSo affectAttributes;
         [SerializeField] private AilmentBlobRootBaked root;
 
         public override int id => root.stackGroupId;
